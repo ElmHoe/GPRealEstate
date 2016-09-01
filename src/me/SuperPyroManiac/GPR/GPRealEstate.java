54e3bcda00e5e4746a928d570c047d2b50c91d3b
SuperPyroManiac <randellkahley@gmail.com>
2016-08-31 22:37:03 -0600

    Revert "Version 1.5.0 Bugfixes."
    This reverts commit c4484f9babbda3cc229fe408097d7dabb40f97b4.

--- src/me/SuperPyroManiac/GPR/GPRealEstate.java
+++ src/me/SuperPyroManiac/GPR/GPRealEstate.java
@@ -69,7 +69,7 @@ public class GPRealEstate extends JavaPlugin {
     
     public boolean onCommand(CommandSender sender, Command command, String label, String[] args) {
     	
-    	if(command.getName().equalsIgnoreCase("gpre") && sender.hasPermission("gprealestate.admin")){
+    	if(command.getName().equalsIgnoreCase("gpre")){
     		
     		if(args.length == 0){
     			sender.sendMessage(dataStore.chatPrefix + ChatColor.RED + "Unknown command function.");
@@ -93,10 +93,6 @@ public class GPRealEstate extends JavaPlugin {
     			
     		}
     		
-    	}
-    	else {
-    		sender.sendMessage(ChatColor.RED + "You do not have permissions to use this command.");
-			return false;
     	}
     	
     	return false;
